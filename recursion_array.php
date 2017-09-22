c205a630f870704763e3193896e24427853eea5b
zengji <zengji@didichuxing.com>
2017-09-22 20:58:11 +0800

    add 递归

---
+++ recursion_array.php
@@ -0,0 +1,31 @@
+<?php
+
+function changedToUnderlineKey(&$arr){
+    if(! is_array($arr) || empty($arr)){
+        return $arr;
+    }
+
+    foreach ($arr as $k => &$v){
+        $kk = str_replace("-", "_", $k);
+        changedToUnderline($v);
+        if($kk != $k ){
+            unset($arr[$k]);
+            $arr[$kk]=$v;
+        }
+    }
+    return $arr;
+}
+
+
+$arr= array(
+    "31234-241"=> 909009,
+    "2342-3242"=>array(
+        "weqr-"=>1,
+        "1234"=> 90,
+        "as-ew"=> array(
+            "24-234" => 90,
+        )
+    ),
+);
+
+var_dump(changedToUnderline($arr));
